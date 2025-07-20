8c177a6147b9ff2fd8993d4ad4423fd8aca9c429
codebyoshama <162036964+codebyoshama@users.noreply.github.com>
2025-07-20 16:23:29 -0500

    first commit

---
+++ src/ClaasAndObject/Car.java
@@ -0,0 +1,18 @@
+package oop;
+
+
+public class Car {
+
+    // Properties
+
+    public String color;
+    public int speed;
+
+
+    public void drive() {
+        System.out.println("Car is driving");
+    }
+
+
+
+}
---
+++ src/Inheritance/Dog.java
@@ -0,0 +1,21 @@
+package Inheritance;
+
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+
+
+}
+
+public class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog bark");
+    }
+}
+
+
+
+
+
